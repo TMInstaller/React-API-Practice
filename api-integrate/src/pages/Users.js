e9602d9ad0c1f3502d051e610bcf733016fde9fe
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-19 13:51:13 +0900

    feat : API 재호출 버튼 추가

--- api-integrate/src/pages/Users.js
+++ api-integrate/src/pages/Users.js
@@ -6,36 +6,40 @@ function Users() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
+  const fetchUsers = async () => {
+    try {
+      // 요청이 시작 할 때에는 error와 users를 초기화하고
+      setError(null);
+      setUsers(null);
+      // loading 상태를 true로 바꾼다
+      setLoading(true);
+      const response = await axios.get(
+        // 이 부분에 들어가는 api의 주소가 다르면 에러를 호출합니다
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      setUsers(response.data);
+    } catch (e) {
+      setError(e);
+    }
+    setLoading(false);
+  };
   useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        // 요청이 시작 할 때에는 error와 users를 초기화하고
-        setError(null);
-        setUsers(null);
-        // loading 상태를 true로 바꾼다
-        setLoading(true);
-        const response = await axios.get(
-          "https://jsonplaceholder.typicode.com/users"
-        );
-        setUsers(response.data);
-      } catch (e) {
-        setError(e);
-      }
-      setLoading(false);
-    };
     fetchUsers();
   }, []);
   if (loading) return <div>로딩중...</div>;
   if (error) return <div>에러가 발생했습니다</div>;
   if (!users) return null;
   return (
-    <ul>
-      {users.map((user) => (
-        <li key={user.id}>
-          {user.username}({user.name})
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.username}({user.name})
+          </li>
+        ))}
+      </ul>
+      <button onClick={fetchUsers}>다시 불러오기</button>
+    </>
   );
 }
 
