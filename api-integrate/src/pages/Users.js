45c2146b15515a6fa8e201be881748200bdcdd7b
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-19 14:20:26 +0900

    feat : 데이터 나중에 불러오기

--- api-integrate/src/pages/Users.js
+++ api-integrate/src/pages/Users.js
@@ -1,56 +1,21 @@
-import { useState, useEffect, useReducer } from "react";
 import axios from "axios";
+import useAsync from "./useAsync";
 
-function reducer(state, action) {
-  switch (action.type) {
-    case "LOADING":
-      return {
-        loading: true,
-        data: null,
-        error: null,
-      };
-    case "SUCCESS":
-      return {
-        loading: false,
-        data: action.data,
-        error: null,
-      };
-    case "ERROR":
-      return {
-        loading: false,
-        data: null,
-        error: action.error,
-      };
-    default:
-      throw new Error(`Unhandled action type: ${action.type}`);
-  }
+// useAsync 에서는 Promise 의 결과를 바로 data 에 담기 때문에,
+// 요청을 한 이후 response 에서 data 추출하여 반환하는 함수를 따로 만들었습니다.
+async function getUsers() {
+  const response = await axios.get(
+    "https://jsonplaceholder.typicode.com/users"
+  );
+  return response.data;
 }
 
 function Users() {
-  const [state, dispatch] = useReducer(reducer, {
-    loading: false,
-    data: null,
-    error: null,
-  });
+  const [state, refetch] = useAsync(getUsers, []);
 
-  const fetchUsers = async () => {
-    dispatch({ type: "LOADING" });
-    try {
-      const response = await axios.get(
-        "https://jsonplaceholder.typicode.com/users"
-      );
-      dispatch({ type: "SUCCESS", data: response.data });
-    } catch (e) {
-      dispatch({ type: "ERROR", error: e });
-    }
-  };
-  useEffect(() => {
-    fetchUsers();
-  }, []);
+  const { loading, data: users, error } = state; // state.data 를 users 키워드로 조회
 
-  const { loading, data: users, error } = state; // state.data를 users키워드로 조회
-
-  if (loading) return <div>로딩중...</div>;
+  if (loading) return <div>로딩중..</div>;
   if (error) return <div>에러가 발생했습니다</div>;
   if (!users) return null;
   return (
@@ -58,11 +23,11 @@ function Users() {
       <ul>
         {users.map((user) => (
           <li key={user.id}>
-            {user.username}({user.name})
+            {user.username} ({user.name})
           </li>
         ))}
       </ul>
-      <button onClick={fetchUsers}>다시 불러오기</button>
+      <button onClick={refetch}>다시 불러오기</button>
     </>
   );
 }
