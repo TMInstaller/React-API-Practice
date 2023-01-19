a6bdb07edb9a24804c6ce6a19bd22ab74c722916
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-19 13:59:37 +0900

    feat : useReducer로 요청 상태 관리하기

--- api-integrate/src/pages/Users.js
+++ api-integrate/src/pages/Users.js
@@ -1,31 +1,55 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useReducer } from "react";
 import axios from "axios";
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "LOADING":
+      return {
+        loading: true,
+        data: null,
+        error: null,
+      };
+    case "SUCCESS":
+      return {
+        loading: false,
+        data: action.data,
+        error: null,
+      };
+    case "ERROR":
+      return {
+        loading: false,
+        data: null,
+        error: action.error,
+      };
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+}
+
 function Users() {
-  const [users, setUsers] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [state, dispatch] = useReducer(reducer, {
+    loading: false,
+    data: null,
+    error: null,
+  });
 
   const fetchUsers = async () => {
+    dispatch({ type: "LOADING" });
     try {
-      // 요청이 시작 할 때에는 error와 users를 초기화하고
-      setError(null);
-      setUsers(null);
-      // loading 상태를 true로 바꾼다
-      setLoading(true);
       const response = await axios.get(
-        // 이 부분에 들어가는 api의 주소가 다르면 에러를 호출합니다
         "https://jsonplaceholder.typicode.com/users"
       );
-      setUsers(response.data);
+      dispatch({ type: "SUCCESS", data: response.data });
     } catch (e) {
-      setError(e);
+      dispatch({ type: "ERROR", error: e });
     }
-    setLoading(false);
   };
   useEffect(() => {
     fetchUsers();
   }, []);
+
+  const { loading, data: users, error } = state; // state.data를 users키워드로 조회
+
   if (loading) return <div>로딩중...</div>;
   if (error) return <div>에러가 발생했습니다</div>;
   if (!users) return null;
